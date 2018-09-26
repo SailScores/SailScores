be086d41841c86170dd90435e7dd22808d94d9fc
Jamie Fraser <jamie@widernets.com>
2018-09-25 19:14:54 -0500

    ClubSummary working
    Still need to figure out best way to create viewmodels.

---
+++ SailScores.Web/Controllers/ClubController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SailScores.Core.Services;
+using SailScores.Web.Models.SailScores;
+
+namespace SailScores.Web.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly IClubService _clubService;
+        private readonly IMapper _mapper;
+
+        public ClubController(
+            IClubService clubService,
+            IMapper mapper)
+        {
+            _clubService = clubService;
+            _mapper = mapper;
+        }
+        
+        // GET: Club
+        public async Task<ActionResult> Index(string clubInitials)
+        {
+            ViewData["ClubInitials"] = clubInitials;
+
+            var club = await _clubService.GetFullClub(clubInitials);
+            return View(_mapper.Map<ClubSummaryViewModel>(club));
+        }
+
+    }
+}
---
+++ SailScores.Web/Models/SailScores/ClubSummaryViewModel.cs
@@ -0,0 +1,39 @@
+﻿using SailScores.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SailScores.Web.Models.SailScores
+{
+    public class ClubSummaryViewModel
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public String Name { get; set; }
+        [StringLength(10)]
+        public String Initials { get; set; }
+        public String Description { get; set; }
+        public bool IsHidden { get; set; }
+
+
+        public IList<Fleet> Fleets { get; set; }
+        public IList<Competitor> Competitors { get; set; }
+        public IList<BoatClass> BoatClasses { get; set; }
+        public IList<Season> Seasons { get; set; }
+        public IList<Series> Series { get; set; }
+        public IList<Race> Races { get; set; }
+        public IList<ScoreCode> ScoreCodes { get; set; }
+
+        private DateTime recentCutoff = DateTime.Now.AddDays(-8);
+        public IEnumerable<Race> RecentRaces => Races?.Where(r => r.Date > recentCutoff);
+
+        public IEnumerable<Series> RecentSeries => Series
+                ?.Where(s =>
+                    s.Races
+                    ?.Any(r => r.Date > recentCutoff) ?? false);
+    }
+}
