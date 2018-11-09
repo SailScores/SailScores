cca05c1f140c04e066626d77a1086f5cf2e1ff9f
Jamie Fraser <jamie@widernets.com>
2018-11-08 18:58:00 -0600

    Creating generic client

---
+++ Sailscores.Client.Uwp/Services/SailscoresApiClient.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Sailscores.Client.Uwp.Services.SettingsServices;
+using SailScores.Core.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sailscores.Client.Uwp.Services
+{
+    public class SailscoresApiClient
+    {
+        private SettingsService _settings;
+
+        public SailscoresApiClient(SettingsService settings)
+        {
+            this._settings = settings;
+        }
+
+        public async Task<T> GetAsync<T>(string urlExtension)
+        {
+            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
+
+            Uri requestUri = new Uri(new Uri(_settings.ServerUrl), urlExtension);
+
+            //Send the GET request asynchronously and retrieve the response as a string.
+            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
+            string httpResponseBody = "";
+
+            try
+            {
+                //Send the GET request
+                httpResponse = await httpClient.GetAsync(requestUri);
+                httpResponse.EnsureSuccessStatusCode();
+                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
+
+                var clubs = JsonConvert.DeserializeObject<T>(httpResponseBody);
+                return clubs;
+            }
+            catch (Exception ex)
+            {
+                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
+                throw;
+            }
+        }
+
+        public async Task<List<Club>> GetClubsAsync()
+        {
+            return await GetAsync<List<Club>>("Club");
+        }
+    }
+}
--- Sailscores.Client.Uwp/Services/SailscoresServerService.cs
+++ Sailscores.Client.Uwp/Services/SailscoresServerService.cs
@@ -12,68 +12,28 @@ namespace Sailscores.Client.Uwp.Services
     public class SailscoresServerService
     {
         private SettingsService _settings;
-
+        private SailscoresApiClient _apiClient;
         private List<Club> _clubs;
 
-        public SailscoresServerService(SettingsService settings)
+        public SailscoresServerService(
+            SettingsService settings,
+            SailscoresApiClient apiClient
+            )
         {
             this._settings = settings;
+            this._apiClient = apiClient;
         }
 
         public static SailscoresServerService GetInstance(SettingsService settings)
         {
-            return new SailscoresServerService(settings);
-        }
-
-        public List<Club> GetClubs()
-        {
-            if (_clubs == null)
-            {
-                _clubs = new List<Club>
-                {
-                    new Club{
-                    Id = Guid.Parse("{43D00C5B-4A79-4903-87CA-379DEB90E4D7}"),
-                    Name = "Test Club 1",
-                    Initials = "TCYC"
-                    },
-                    new Club{
-                    Id = Guid.Parse("{A1A7015D-8DA4-4CE5-9C68-3A978F980CFC}"),
-                    Name = "Test Club 2",
-                    Initials = "TEST"
-                    },
-                };
-            }
-            return _clubs;
+            return new SailscoresServerService(settings,
+                new SailscoresApiClient(settings));
         }
 
 
         public async Task<List<Club>> GetClubsAsync()
         {
-            //await Task.Delay(5000);
-            //return GetClubs();
-            Windows.Web.Http.HttpClient httpClient = new Windows.Web.Http.HttpClient();
-
-            Uri requestUri = new Uri(new Uri(_settings.ServerUrl), "Club");
-
-            //Send the GET request asynchronously and retrieve the response as a string.
-            Windows.Web.Http.HttpResponseMessage httpResponse = new Windows.Web.Http.HttpResponseMessage();
-            string httpResponseBody = "";
-
-            try
-            {
-                //Send the GET request
-                httpResponse = await httpClient.GetAsync(requestUri);
-                httpResponse.EnsureSuccessStatusCode();
-                httpResponseBody = await httpResponse.Content.ReadAsStringAsync();
-
-                var clubs = JsonConvert.DeserializeObject<List<Club>>(httpResponseBody);
-                return clubs;
-            }
-            catch (Exception ex)
-            {
-                httpResponseBody = "Error: " + ex.HResult.ToString("X") + " Message: " + ex.Message;
-                throw;
-            }
+            return await _apiClient.GetClubsAsync();
         }
 
     }
