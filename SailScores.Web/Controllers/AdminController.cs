9280b69e629ea3ec7a7257a09c7f5c4136c6fcad
Jamie Fraser <jamie@widernets.com>
2019-01-20 20:32:28 -0600

    Include beginning of Club Admin Page

---
+++ SailScores.Web/Controllers/AdminController.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SailScores.Core.Services;
+using SailScores.Web.Models.SailScores;
+
+namespace SailScores.Web.Controllers
+{
+    [Authorize]
+    public class AdminController : Controller
+    {
+
+        private readonly IClubService _clubService;
+        private readonly IMapper _mapper;
+
+        public AdminController(
+            IClubService clubService,
+            IMapper mapper)
+        {
+            _clubService = clubService;
+            _mapper = mapper;
+        }
+
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Admin/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Admin/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Edit/5
+        public async Task<ActionResult> Edit(string clubInitials)
+        {
+            ViewData["ClubInitials"] = clubInitials;
+
+            var club = await _clubService.GetFullClub(clubInitials);
+            return View(_mapper.Map<AdminViewModel>(club));
+        }
+
+        // POST: Admin/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Admin/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
