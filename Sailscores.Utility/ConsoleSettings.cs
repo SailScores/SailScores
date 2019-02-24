d61100558df479c50636ef306db62ff236ace8c2
Jamie Fraser <jamie@widernets.com>
2019-02-24 13:59:16 -0600

    Tweaks

--- Sailscores.Utility/ConsoleSettings.cs
+++ Sailscores.Utility/ConsoleSettings.cs
@@ -50,7 +50,7 @@ namespace SailScores.Utility
                 if (String.IsNullOrWhiteSpace(_password))
                 {
                     Console.Write("Password? > ");
-                    _password = Console.ReadLine();
+                    _password = ReadPassword();
                 }
                 return _password;
             }
@@ -58,6 +58,40 @@ namespace SailScores.Utility
             {
                 _password = value;
             }
+        }
+        public static string ReadPassword()
+        {
+            string password = "";
+            ConsoleKeyInfo info = Console.ReadKey(true);
+            while (info.Key != ConsoleKey.Enter)
+            {
+                if (info.Key != ConsoleKey.Backspace)
+                {
+                    Console.Write("*");
+                    password += info.KeyChar;
+                }
+                else if (info.Key == ConsoleKey.Backspace)
+                {
+                    if (!string.IsNullOrEmpty(password))
+                    {
+                        // remove one character from the list of password characters
+                        password = password.Substring(0, password.Length - 1);
+                        // get the location of the cursor
+                        int pos = Console.CursorLeft;
+                        // move the cursor to the left by one character
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                        // replace it with space
+                        Console.Write(" ");
+                        // move the cursor to the left by one character again
+                        Console.SetCursorPosition(pos - 1, Console.CursorTop);
+                    }
+                }
+                info = Console.ReadKey(true);
+            }
+
+            // add a new line because user pressed enter at the end of their password
+            Console.WriteLine();
+            return password;
         }
     }
 }
