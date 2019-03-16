47b7b7db2bed1d6104d7ed70d211680518487d30
Jamie Fraser <jamie@widernets.com>
2019-03-16 10:57:33 -0500

    Sitemap builder

---
+++ SailScores.Web/Models/Sitemap/ChangeFrequency.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SailScores.Web.Models.Sitemap
+{
+    public enum ChangeFrequency
+    {
+        Always,
+        Hourly,
+        Daily,
+        Weekly,
+        Monthly,
+        Yearly,
+        Never
+    }
+}
---
+++ SailScores.Web/Models/Sitemap/SitemapUrl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SailScores.Web.Models.Sitemap
+{
+    public class SitemapUrl
+    {
+        public string Url { get; set; }
+        public DateTime? Modified { get; set; }
+        public ChangeFrequency? ChangeFrequency { get; set; }
+        public double? Priority { get; set; }
+    }
+}
