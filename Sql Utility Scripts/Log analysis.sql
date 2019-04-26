8bbc1fe02c79cd171ab1cc6a5368fe9820ca19f5
Jamie Fraser <jamie@widernets.com>
2019-04-25 20:23:49 -0500

    Race Create fixes

--- Sql Utility Scripts/Log analysis.sql
+++ Sql Utility Scripts/Log analysis.sql
@@ -1,14 +1,18 @@
 
-Select top 20
+Select top 100
 Message,
-Level,
+--Level,
 TimeStamp,
 Properties.value('(/properties/property[@key="ElapsedMilliseconds"])[1]', 'varchar(50)'),
-Properties.value('(/properties/property[@key="SourceContext"])[1]', 'nvarchar(max)'),
-Properties.value('(/properties/property[@key="Status"])[1]', 'nvarchar(20)')
+--Properties.value('(/properties/property[@key="SourceContext"])[1]', 'nvarchar(max)'),
+Properties.value('(/properties/property[@key="StatusCode"])[1]', 'nvarchar(20)'),
+Properties.value('(/properties/property[@key="RequestPath"])[1]', 'nvarchar(80)')
 ,
 Properties
  from Logs
+ WHERE
+ 
+Properties.value('(/properties/property[@key="StatusCode"])[1]', 'nvarchar(20)') <> '307'
 -- WHERE
 --
 --Properties.value('(/properties/property[@key="SourceContext"])[1]', 'varchar(100)') in
@@ -16,3 +20,6 @@ Properties
 order by Id  desc
 
 
+
+
+Select * from AspNetUsers
