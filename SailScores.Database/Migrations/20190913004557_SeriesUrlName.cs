bb355b155eb137732333c6a8af5ac3284e379e16
Jamie Fraser <jamie@widernets.com>
2019-09-12 19:55:22 -0500

    EF migration for Series UrlName

---
+++ SailScores.Database/Migrations/20190913004557_SeriesUrlName.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SailScores.Database.Migrations
+{
+    public partial class SeriesUrlName : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UrlName",
+                table: "Series",
+                maxLength: 200,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UrlName",
+                table: "Series");
+        }
+    }
+}
