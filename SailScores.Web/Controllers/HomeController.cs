29d65270ad8315d49cd7029458e0b41237039c24
Jamie Fraser <jamie@widernets.com>
2019-10-15 19:06:16 -0500

    404 page

--- SailScores.Web/Controllers/HomeController.cs
+++ SailScores.Web/Controllers/HomeController.cs
@@ -60,11 +60,5 @@ namespace SailScores.Web.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
