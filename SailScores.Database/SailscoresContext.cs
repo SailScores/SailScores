fbd546de5ad96c122902d60fec886ddc650993a9
Jamie Fraser <jamie@widernets.com>
2019-10-24 21:23:56 -0500

    Misc Fixes

--- SailScores.Database/SailscoresContext.cs
+++ SailScores.Database/SailscoresContext.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -41,6 +42,18 @@ namespace SailScores.Database
         protected override void
             OnModelCreating(ModelBuilder modelBuilder)
         {
+            foreach (var property in modelBuilder.Model.GetEntityTypes()
+                .SelectMany(t => t.GetProperties())
+                .Where(p => p.ClrType == typeof(decimal) || p.ClrType == typeof(decimal?)))
+            {
+                // EF Core 1 & 2
+                property.Relational().ColumnType = "decimal(18, 2)";
+
+                // EF Core 3
+                //property.SetColumnType("decimal(18, 6)");
+            }
+
+
             modelBuilder.Entity<CompetitorFleet>()
                 .HasKey(x => new {x.CompetitorId, x.FleetId});
             modelBuilder.Entity<SeriesRace>()
