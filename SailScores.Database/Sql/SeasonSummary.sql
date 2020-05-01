fc0fce2f336912e17d49a565ee048498790a5064
Jamie Fraser <jamie@widernets.com>
2020-04-30 20:58:49 -0500

    SQL optimizations

--- SailScores.Database/Sql/SeasonSummary.sql
+++ SailScores.Database/Sql/SeasonSummary.sql
@@ -21,37 +21,24 @@ SET @CompetitorId = (
         )
 
 SELECT
-    c.Id AS CompetitorId,
     Seasons.Name AS SeasonName,
     Seasons.[Start] AS [SeasonStart],
     Seasons.[End] AS [SeasonEnd],
-    count(distinct s.RaceId) AS RaceCount,
-    AVG(CAST(s.Place AS FLOAT)) AS AverageFinishRank,
+    count(distinct RaceResults.Id) AS RaceCount,
+    AVG(CAST(RaceResults.Place AS FLOAT)) AS AverageFinishRank,
     count(DISTINCT RacedDate) AS DaysRaced,
-    -- CASE WHEN RaceResults.FinisherCount = 1 THEN Null ELSE
-    --    CONVERT(Decimal(7,4),RaceResults.FinisherCount - RaceResults.Place) /
-    --    CONVERT(Decimal(7,4), RaceResults.FinisherCount - 1)
-    --    END as percentile
-
-    SUM(RaceResults.FinisherCount) AS RaceFinishers,
     SUM(RaceResults.FinisherCount - 1) AS BoatsRacedAgainst,
     SUM(RaceResults.FinisherCount - RaceResults.Place ) AS BoatsBeat
 FROM
-    Seasons
-    INNER JOIN
-    Competitors c
-    on c.Id = @CompetitorId -- unconventional, but awesome
-    AND Seasons.ClubId = @ClubId
-    LEFT OUTER JOIN Races r
-    ON r.[Date] >= Seasons.[Start] AND r.[Date] <= Seasons.[End]
-    AND r.ClubId = @ClubId
-    LEFT OUTER JOIN
-    Scores s
-    ON c.Id = s.CompetitorId
-    and r.Id = s.RaceId
+(SELECT TOP 2
+        *
+    FROM Seasons
+    WHERE Seasons.[Start] < GETDATE()
+        AND Seasons.ClubId = @ClubId
+    ORDER BY [Start] DESC) AS Seasons
     LEFT OUTER JOIN
     (
-SELECT
+   SELECT
         r.Id,
         r.Date as RacedDate,
         COUNT(*) FinisherCount,
@@ -69,25 +56,10 @@ SELECT
         ON r.Id = racerScore.RaceId
             AND racerScore.CompetitorId = @CompetitorId
     WHERE
-ISNULL(s.Code, '') = ''
+        ISNULL(s.Code, '') = ''
         AND ISNULL(racerScore.Place,0) <> 0
-    GROUP BY r.Id,
-    r.Date,
-racerScore.Place
-) AS RaceResults
-    ON RaceResults.Id = r.Id
-WHERE
-ISNULL(s.Code, '') = ''
-    AND
-    c.Id = @CompetitorId
-    -- and ( Place is null or Place = 0 )
-    AND seasons.ID IN (SELECT TOP 2
-        Seasons.ID
-    FROM Seasons
-    WHERE Seasons.[Start] < GETDATE()
-        AND Seasons.ClubId = @ClubId
-    ORDER BY [Start] DESC)
-    --Not in a series that Shouldn't be counted toward season stats
+		AND r.ClubId = @ClubId
+		    --Not in a series that Shouldn't be counted toward season stats
     AND (r.ID IS NULL OR r.Id NOT IN ( SELECT r2.Id
     FROM
         Races r2
@@ -98,9 +70,13 @@ ISNULL(s.Code, '') = ''
     WHERE s2.ExcludeFromCompetitorStats = 1
 
 ))
-
+    GROUP BY r.Id,
+    r.Date,
+racerScore.Place
+) AS RaceResults
+    ON 
+	RaceResults.RacedDate >= Seasons.[Start] AND RaceResults.RacedDate <= Seasons.[End]
 GROUP BY
-   c.Id,
    Seasons.Name,
    Seasons.[Start],
    Seasons.[End]
