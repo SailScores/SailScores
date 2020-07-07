c4a74fff2e0a6f78417f0facfe50bccf84174747
Jamie Fraser <jamie@widernets.com>
2020-07-06 21:48:53 -0500

    Update club copy sql

--- Sql Utility Scripts/Copy a club.sql
+++ Sql Utility Scripts/Copy a club.sql
@@ -1,88 +1,123 @@
 
-Declare @NewClubId UNIQUEIDENTIFIER
-set @NewClubId = NEWID()
+DECLARE @NewClubId UNIQUEIDENTIFIER
+SET @NewClubId = NEWID()
 
-Declare @FromInitials NVARCHAR(20)
-SET @FromInitials = 'LHYC'
+DECLARE @FromInitials NVARCHAR(20)
+SET @FromInitials = 'SBYCTEST'
 
 -- Select *
 -- from Clubs
 -- where Initials = @FromInitials
 
-Declare @FromClubId UNIQUEIDENTIFIER
-SET @FromClubId = (Select Id
-from Clubs
-where Initials = @FromInitials)
+DECLARE @FromClubId UNIQUEIDENTIFIER
+SET @FromClubId = (SELECT Id
+FROM Clubs
+WHERE Initials = @FromInitials)
 
 PRINT N'COPYING CLUB'
 PRINT N'From ID: ' + CONVERT(NVARCHAR(100), @FromClubId) + ' To ID: ' + CONVERT(NVARCHAR(100), @NewClubId)
 
-INSERT into Clubs
+INSERT INTO Clubs
     (
     Id,
     Name,
     Initials,
     Description,
     IsHidden,
-    Url
+    Url,
+    Locale,
+    ShowClubInResults
     )
-Select
+SELECT
     @NewClubId,
     Name + '2',
     Initials + '2',
     Description,
     1,
-    url
-from Clubs
-where Id = @FromClubId
+    url,
+    Locale,
+    ShowClubInResults
+FROM Clubs
+WHERE Id = @FromClubId
 
 PRINT N'Club created. Copying permissions'
 
-Insert into UserPermissions
+INSERT INTO UserPermissions
     ( Id,
     UserEmail,
     ClubId,
     CanEditAllClubs)
-Select NEWID(), UserEmail, @NewClubId, CanEditAllClubs
-from UserPermissions
-where ClubId = @FromClubId
+SELECT NEWID(), UserEmail, @NewClubId, CanEditAllClubs
+FROM UserPermissions
+WHERE ClubId = @FromClubId
+
+
+PRINT N'Club created. Copying weather settings'
+CREATE TABLE #IdTranslation
+(
+    FromId UNIQUEIDENTIFIER,
+    ToId UNIQUEIDENTIFIER,
+    ConvertHistoricalJson BIT NULL
+)
+
+INSERT INTO #IdTranslation
+    (FromId, ToId)
+SELECT Id, NEWID()
+FROM WeatherSettings
+WHERE Id = (SELECT WeatherSettingsId
+FROM Clubs
+WHERE Id = @FromClubId)
+
+INSERT INTO WeatherSettings
+    (Id, Latitude, Longitude, TemperatureUnits, WindSpeedUnits)
+SELECT #IdTranslation.ToId, Latitude, Longitude, TemperatureUnits, WindSpeedUnits
+
+FROM WeatherSettings
+INNER JOIN #IdTranslation
+ON WeatherSettings.Id = #IdTranslation.FromId
+
+
+UPDATE Clubs
+SET WeatherSettingsId = (SELECT #IdTranslation.ToId
+FROM WeatherSettings ws2
+    INNER JOIN #IdTranslation
+    ON ws2.Id = #IdTranslation.FromId
+WHERE Id = (SELECT WeatherSettingsId
+FROM Clubs c2
+WHERE c2.Id = @FromClubId))
+WHERE Clubs.Id = @NewClubId
+
 
 
 
 PRINT N'Copying Boat Classes'
-Create TABLE #IdTranslation
-(
-    FromId UNIQUEIDENTIFIER,
-    ToId UNIQUEIDENTIFIER,
-    ConvertHistoricalJson BIT Null
-    )
 
-Insert into #IdTranslation
+INSERT INTO #IdTranslation
     (FromId, ToId)
-Select Id, NEWID()
-from BoatClasses
-where ClubId = @FromClubId
+SELECT Id, NEWID()
+FROM BoatClasses
+WHERE ClubId = @FromClubId
 
-Insert into BoatClasses
+INSERT INTO BoatClasses
     (Id,
     ClubId,
     Name,
     Description)
-Select trans.ToId, @NewClubId, Name, Description
-from BoatClasses
-    inner join #IdTranslation trans
-    on BoatClasses.Id = trans.FromId
+SELECT trans.ToId, @NewClubId, Name, Description
+FROM BoatClasses
+    INNER JOIN #IdTranslation trans
+    ON BoatClasses.Id = trans.FromId
 WHERE ClubId = @FromClubId
 
 PRINT N'Copying Fleets'
 
-Insert into #IdTranslation
+INSERT INTO #IdTranslation
     (FromId, ToId)
-Select Id, NEWID()
-from Fleets
-where ClubId = @FromClubId
+SELECT Id, NEWID()
+FROM Fleets
+WHERE ClubId = @FromClubId
 
-Insert Into Fleets
+INSERT INTO Fleets
     (
     Id,
     ClubId,
@@ -90,25 +125,29 @@ Insert Into Fleets
     [Description],
     FleetType,
     IsHidden,
-    ShortName
+    ShortName,
+    NickName,
+    IsActive
     )
-Select
+SELECT
     trans.ToId,
-    @NewClubId, Name, Description, FleetType, IsHidden, ShortName
-from Fleets
-    inner join #IdTranslation trans
-    on trans.FromId = Fleets.Id
-where ClubId = @FromClubId
+    @NewClubId, Name, Description, FleetType, IsHidden, ShortName,
+    NickName,
+    IsActive
+FROM Fleets
+    INNER JOIN #IdTranslation trans
+    ON trans.FromId = Fleets.Id
+WHERE ClubId = @FromClubId
 
 PRINT N'Copying Competitors'
 
-Insert into #IdTranslation
+INSERT INTO #IdTranslation
     (FromId, ToId , ConvertHistoricalJson)
-Select Id, NEWID() , 1
-from Competitors
-where ClubId = @FromClubId
+SELECT Id, NEWID() , 1
+FROM Competitors
+WHERE ClubId = @FromClubId
 
-Insert into Competitors
+INSERT INTO Competitors
     (
     Id,
     ClubId,
@@ -117,240 +156,354 @@ Insert into Competitors
     AlternativeSailNumber,
     BoatName,
     Notes,
-    BoatClassId
+    BoatClassId,
+    IsActive,
+    HomeClubName
     )
-Select
+SELECT
     trans.ToId,
     @NewClubId,
     Competitors.Name, SailNumber, AlternativeSailNumber, BoatName, Notes,
-classTrans.ToId
-from Competitors
-    inner join #IdTranslation trans
-    on Competitors.Id = trans.FromId
-    inner join #IdTranslation classTrans
-    on classTrans.FromId = Competitors.BoatClassId
-where Competitors.ClubId = @FromClubId
+    classTrans.ToId,
+    IsActive,
+    HomeClubName
+FROM Competitors
+    INNER JOIN #IdTranslation trans
+    ON Competitors.Id = trans.FromId
+    INNER JOIN #IdTranslation classTrans
+    ON classTrans.FromId = Competitors.BoatClassId
+WHERE Competitors.ClubId = @FromClubId
 
 PRINT N'Copying Competitor Fleet'
-Insert into CompetitorFleet
+INSERT INTO CompetitorFleet
     (CompetitorId,
     FleetId)
-Select compTrans.ToId,
+SELECT compTrans.ToId,
     fleetTrans.FromId
 FROM
     CompetitorFleet cf
-    inner join Fleets f
-    on f.Id = cf.FleetId
-    inner join #IdTranslation compTrans
-    on compTrans.FromId = cf.CompetitorId
-    inner join #IdTranslation fleetTrans
-    on fleetTrans.FromId = cf.FleetId
+    INNER JOIN Fleets f
+    ON f.Id = cf.FleetId
+    INNER JOIN #IdTranslation compTrans
+    ON compTrans.FromId = cf.CompetitorId
+    INNER JOIN #IdTranslation fleetTrans
+    ON fleetTrans.FromId = cf.FleetId
 
 PRINT N'Copying Fleet Boat Class'
 
-Insert into FleetBoatClass
+INSERT INTO FleetBoatClass
     (
     FleetId,
     BoatClassId
     )
-Select
+SELECT
     fleetTrans.ToId,
     classTrans.ToId
 FROM
     FleetBoatClass fbc
-    inner join #IdTranslation fleetTrans
-    on fleetTrans.FromId = fbc.FleetId
-    inner join #IdTranslation classTrans
-    on classTrans.FromId = fbc.BoatClassId
+    INNER JOIN #IdTranslation fleetTrans
+    ON fleetTrans.FromId = fbc.FleetId
+    INNER JOIN #IdTranslation classTrans
+    ON classTrans.FromId = fbc.BoatClassId
 
 PRINT N'Copying Seasons'
 
-Insert into Seasons
+INSERT INTO Seasons
     (
-    Id, ClubId, Name, [Start], [End]
+    Id, ClubId, Name, [Start], [End],
+    UrlName
     )
-Select NEWID(), @NewClubId, Name, [Start], [End]
-from Seasons
-where ClubId = @FromClubId
+SELECT NEWID(), @NewClubId, Name, [Start], [End],
+UrlName
+FROM Seasons
+WHERE ClubId = @FromClubId
+
+
+
+PRINT N'Copying ScoringSystems'
+
+INSERT INTO #IdTranslation
+    (FromId, ToId , ConvertHistoricalJson)
+SELECT Id, NEWID() , 1
+FROM ScoringSystems
+WHERE ClubId = @FromClubId
+
+INSERT INTO ScoringSystems
+    (
+    Id, ClubId, Name, [DiscardPattern], [ParentSystemId], [ParticipationPercent], OwningClubId
+    )
+SELECT trans.ToId, @NewClubId, ss.Name, [DiscardPattern], [ParentSystemId], [ParticipationPercent], OwningClubId
+FROM ScoringSystems ss
+
+    LEFT OUTER JOIN #IdTranslation AS trans
+    ON ss.Id = trans.FromId
+WHERE ss.ClubId = @FromClubId
+
+
+UPDATE Clubs
+SET DefaultScoringSystemId = (SELECT #IdTranslation.ToId
+FROM ScoringSystems ss2
+    INNER JOIN #IdTranslation
+    ON ss2.Id = #IdTranslation.FromId
+WHERE Id = (SELECT DefaultScoringSystemId
+FROM Clubs c2
+WHERE c2.Id = @FromClubId))
+WHERE Clubs.Id = @NewClubId
+
+
+PRINT N'Copying Score Codes'
+
+INSERT INTO ScoreCodes
+    (
+    Id,
+    Name, [Description],
+    PreserveResult, Discardable,
+    [Started], FormulaValue,
+    AdjustOtherScores, CameToStart,
+    Finished, Formula,
+    ScoreLike, ScoringSystemId )
+SELECT NEWID(), ScoreCodes.Name, [Description], PreserveResult, Discardable, [Started], FormulaValue,
+    AdjustOtherScores, CameToStart, Finished, Formula, ScoreLike,
+    (SELECT Id
+    FROM ScoringSystems
+    WHERE ClubId = @NewClubId AND Name = s.Name)
+FROM ScoreCodes
+    INNER JOIN ScoringSystems s
+    ON s.Id = ScoreCodes.ScoringSystemId
+WHERE s.ClubId = @FromClubId
+
+
 
 PRINT N'Copying Series'
 
-Insert into #IdTranslation
+INSERT INTO #IdTranslation
     (FromId, ToId, ConvertHistoricalJson)
-Select Id, NEWID(), 1
-from Series
-where ClubId = @FromClubId
+SELECT Id, NEWID(), 1
+FROM Series
+WHERE ClubId = @FromClubId
 
-Insert into Series
+INSERT INTO Series
     (
     Id,
     ClubId,
     Name,
     [Description],
-    SeasonId
+    SeasonId,
+    IsImportantSeries,
+    UpdatedDateUtc,
+    ScoringSystemId,
+    ResultsLocked,
+TrendOption,
+FleetId,
+PreferAlternativeSailNumbers,
+UrlName,
+ExcludeFromCompetitorStats
     )
-Select trans.ToId, @NewClubId, series.Name, Description,
-    (Select Id
-    from Seasons
-    where [Start] = s.Start and ClubId = @NewClubId)
-from Series
-    left outer join Seasons as s
-    on series.SeasonId = s.Id
-    inner join #IdTranslation trans
-    on Series.Id = trans.FromId
-where Series.ClubId = @FromClubId
+SELECT trans.ToId, @NewClubId, series.Name, Description,
+    (SELECT Id
+    FROM Seasons
+    WHERE [Start] = s.Start AND ClubId = @NewClubId),
+    IsImportantSeries,
+    UpdatedDateUtc,
+    sstrans.ToId,
+    ResultsLocked,
+TrendOption,
+FleetId,
+PreferAlternativeSailNumbers,
+Series.UrlName,
+ExcludeFromCompetitorStats
+FROM Series
+    LEFT OUTER JOIN Seasons AS s
+    ON series.SeasonId = s.Id
+    INNER JOIN #IdTranslation trans
+    ON Series.Id = trans.FromId
+    left outer join #IdTranslation AS sstrans
+    on sstrans.FromId = series.ScoringSystemId
+WHERE Series.ClubId = @FromClubId
 
-PRINT N'Copying Scoring System'
-
-Insert into ScoringSystems
-    (Id, ClubId, Name, DiscardPattern)
-Select NewId(), @NewClubId, Name, DiscardPattern
-from ScoringSystems
-where ClubId = @FromClubId
 
 
-Insert into #IdTranslation
+PRINT N'Copying Race Weather'
+
+INSERT INTO #IdTranslation
     (FromId, ToId , ConvertHistoricalJson)
-Select Id, NEWID() , 1
-from Races
-where ClubId = @FromClubId
+SELECT Id, NEWID() , 1
+FROM Weather
+WHERE Id in (Select WeatherId from Races where ClubId = @FromClubId)
+
+INSERT INTO Weather
+    (  [Id]
+      ,[Description]
+      ,[Icon]
+      ,[TemperatureString]
+      ,[TemperatureDegreesKelvin]
+      ,[WindSpeedString]
+      ,[WindSpeedMeterPerSecond]
+      ,[WindDirectionString]
+      ,[WindDirectionDegrees]
+      ,[WindGustString]
+      ,[WindGustMeterPerSecond]
+      ,[Humidity]
+      ,[CloudCoverPercent]
+      ,[CreatedDateUtc]
+      )
+SELECT
+    trans.ToId
+      ,[Description]
+      ,[Icon]
+      ,[TemperatureString]
+      ,[TemperatureDegreesKelvin]
+      ,[WindSpeedString]
+      ,[WindSpeedMeterPerSecond]
+      ,[WindDirectionString]
+      ,[WindDirectionDegrees]
+      ,[WindGustString]
+      ,[WindGustMeterPerSecond]
+      ,[Humidity]
+      ,[CloudCoverPercent]
+      ,[CreatedDateUtc]
+FROM Weather
+    INNER JOIN #IdTranslation trans
+    ON Id = trans.FromId
+
+
+
+INSERT INTO #IdTranslation
+    (FromId, ToId , ConvertHistoricalJson)
+SELECT Id, NEWID() , 1
+FROM Races
+WHERE ClubId = @FromClubId
 
 PRINT N'Copying Races'
 
-Insert into Races
+INSERT INTO Races
     (
-    Id, ClubId, Name, [Date], [Order], [Description], FleetId, [State]
+    Id, ClubId, Name, [Date], [Order], [Description], FleetId, [State], TrackingUrl, UpdatedDateUtc, WeatherId
     )
-Select trans.ToId, @NewClubId, Races.Name, [Date], [Order], Races.[Description],
-    (Select Id
-    from Fleets
-    where ClubId = @NewClubId and Name = f.Name), [State]
-from Races
-    left outer join Fleets f
-    on f.Id = races.FleetId
-    left outer join #IdTranslation as trans
-    on Races.Id = trans.FromId
-where races.ClubId = @FromClubId
+SELECT trans.ToId, @NewClubId, Races.Name, [Date], [Order], Races.[Description],
+    (SELECT Id
+    FROM Fleets
+    WHERE ClubId = @NewClubId AND Name = f.Name), [State],
+    TrackingUrl, UpdatedDateUtc,
+    weathertrans.ToId
+FROM Races
+    LEFT OUTER JOIN Fleets f
+    ON f.Id = races.FleetId
+    LEFT OUTER JOIN #IdTranslation AS trans
+    ON Races.Id = trans.FromId
+    left outer join #IdTranslation AS weathertrans
+    on weathertrans.FromId = races.WeatherId
+WHERE races.ClubId = @FromClubId
 
-PRINT N'Copying Score Codes'
-
-Insert into ScoreCodes
-    (
-    Id, ClubId, Name, [Description], PreserveResult, Discardable, [Started], FormulaValue, AdjustOtherScores, CameToStart, Finished, Formula,ScoreLike, ScoringSystemId )
-SELECT NEWID(), @NewClubId, ScoreCodes.Name, [Description], PreserveResult, Discardable, [Started], FormulaValue, AdjustOtherScores, CameToStart, Finished, Formula, ScoreLike,
-    (Select Id
-    from ScoringSystems
-    where ClubId = @NewClubId and Name = s.Name)
-FROM ScoreCodes
-    left outer join ScoringSystems s
-    on s.Id = ScoreCodes.ScoringSystemId
-where ScoreCodes.ClubId = @FromClubId
 
 PRINT N'Copying Scores'
 
-Insert into Scores
-    ( Id, CompetitorId, RaceId, Place, Code)
-Select NewId(),
+INSERT INTO Scores
+    ( Id, CompetitorId, RaceId, Place, Code, CodePoints)
+SELECT NewId(),
     compTrans.ToId,
     racetrans.ToId,
-    Place, Code
-from Scores
-    left outer join #IdTranslation raceTrans
-    on raceTrans.FromId = Scores.RaceId
-    left outer join #IdTranslation compTrans
-    on compTrans.FromId = Scores.CompetitorId
-where Scores.RaceId in (Select Id
-from Races
-where ClubId = @FromClubId )
+    Place, Code,
+    CodePoints
+FROM Scores
+    LEFT OUTER JOIN #IdTranslation raceTrans
+    ON raceTrans.FromId = Scores.RaceId
+    LEFT OUTER JOIN #IdTranslation compTrans
+    ON compTrans.FromId = Scores.CompetitorId
+WHERE Scores.RaceId IN (SELECT Id
+FROM Races
+WHERE ClubId = @FromClubId )
 
 PRINT N'Copying Series Race assignments'
 
-Insert into SeriesRace
+INSERT INTO SeriesRace
     ( RaceId, SeriesId)
-Select
+SELECT
     raceTrans.ToId,
     seriesTrans.ToId
-from SeriesRace
-    inner join #IdTranslation raceTrans
-    on RaceId = FromId
-    inner join #IdTranslation seriesTrans
-    on SeriesId = seriesTrans.FromId
+FROM SeriesRace
+    INNER JOIN #IdTranslation raceTrans
+    ON RaceId = FromId
+    INNER JOIN #IdTranslation seriesTrans
+    ON SeriesId = seriesTrans.FromId
+
+
 
 
 PRINT N'Copying Historical result cache'
 
 --- The big weird part:
 
-Insert into #IdTranslation
+INSERT INTO #IdTranslation
     (fromId, ToId)
-Select
+SELECT
     HistoricalResults.Id, NewId()
-from HistoricalResults inner join Series
-    on HistoricalResults.SeriesId = Series.Id
-where Series.ClubId = @FromClubId
+FROM HistoricalResults INNER JOIN Series
+    ON HistoricalResults.SeriesId = Series.Id
+WHERE Series.ClubId = @FromClubId
 
-Insert into HistoricalResults
+INSERT INTO HistoricalResults
     (Id,
     SeriesId,
     IsCurrent,
     Results,
     Created)
-Select
+SELECT
     historyTrans.ToId,
     seriestrans.ToId,
     IsCurrent,
     Results,
     Created
-from HistoricalResults
-    inner join #IdTranslation historyTrans
-    on HistoricalResults.Id = historyTrans.FromId
-    inner join #IdTranslation seriestrans
-    on SeriesId = seriestrans.FromId
+FROM HistoricalResults
+    INNER JOIN #IdTranslation historyTrans
+    ON HistoricalResults.Id = historyTrans.FromId
+    INNER JOIN #IdTranslation seriestrans
+    ON SeriesId = seriestrans.FromId
 -- Inner join above filters to just Series translated in this execution.
 
 
-DECLARE @runningCount int = 1;
-DECLARE @TotalCount int;
+DECLARE @runningCount INT = 1;
+DECLARE @TotalCount INT;
 PRINT N'Finding and Replacing Ids in cache'
-declare @ErrMsg NVARCHAR(500);
+DECLARE @ErrMsg NVARCHAR(500);
 
-DECLARE @FromId varchar(50),
-   @ToId varchar(50)
+DECLARE @FromId VARCHAR(50),
+   @ToId VARCHAR(50)
 
 SET @TotalCount = (SELECT COUNT(*)
-FROM #IdTranslation  
+FROM #IdTranslation
 WHERE ConvertHistoricalJson = 1  
 )
 
 DECLARE guid_cursor CURSOR FOR   
-SELECT CONVERT(varchar(50), FromId), CONVERT(VARCHAR(50), ToId)  
-FROM #IdTranslation  
-WHERE ConvertHistoricalJson = 1  
-ORDER BY FromId;  
-  
-OPEN guid_cursor  
-  
+SELECT CONVERT(VARCHAR(50), FromId), CONVERT(VARCHAR(50), ToId)
+FROM #IdTranslation
+WHERE ConvertHistoricalJson = 1
+ORDER BY FromId;
+
+OPEN guid_cursor
+
 FETCH NEXT FROM guid_cursor   
-INTO @FromId, @ToId  
-  
+INTO @FromId, @ToId
+
 WHILE @@FETCH_STATUS = 0  
-BEGIN  
+BEGIN
 
-  update hr
-    set hr.Results = REPLACE(hr.Results, @FromId, @ToId)
-    from HistoricalResults as hr
-    where hr.Id in (Select ToId
-    from #IdTranslation)
+    UPDATE hr
+    SET hr.Results = REPLACE(hr.Results, @FromId, @ToId)
+    FROM HistoricalResults AS hr
+    WHERE hr.Id IN (SELECT ToId
+    FROM #IdTranslation)
 
-  SET @runningCount = @runningCount + 1;
-  if(@runningCount % 10 = 0) BEGIN 
-    SET @ErrMsg = CONVERT(VARCHAR(10), @runningCount) + ' / ' + CONVERT(VARCHAR(10), @TotalCount) 
-    RAISERROR ( @ErrMsg, 0, 1) WITH NOWAIT
-  END
+    SET @runningCount = @runningCount + 1;
+    IF(@runningCount % 10 = 0) BEGIN
+        SET @ErrMsg = CONVERT(VARCHAR(10), @runningCount) + ' / ' + CONVERT(VARCHAR(10), @TotalCount)
+        RAISERROR ( @ErrMsg, 0, 1) WITH NOWAIT
+    END
     FETCH NEXT FROM guid_cursor
     INTO @FromId, @ToId
 END
-CLOSE guid_cursor;  
+CLOSE guid_cursor;
 DEALLOCATE guid_cursor;
 
 
@@ -361,7 +514,7 @@ DEALLOCATE guid_cursor;
 
 PRINT N'Dropping Id Translation table'
 
-DROP Table #idTranslation
+DROP TABLE #idTranslation
 
 PRINT N'Done'
 
