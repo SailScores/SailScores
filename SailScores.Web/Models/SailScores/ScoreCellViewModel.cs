a41d7052d757281dd9a718339ec8294ad6bc955e
Jamie Fraser <jamie@widernets.com>
2023-10-31 17:13:08 -0500

    High point chart enhancements

--- SailScores.Web/Models/SailScores/ScoreCellViewModel.cs
+++ SailScores.Web/Models/SailScores/ScoreCellViewModel.cs
@@ -7,6 +7,5 @@ public class ScoreCellViewModel
 {
     public FlatCalculatedScore Result { get; set; } 
     public bool IsPercentSystem { get; set; } 
-    public bool IsHighPointSystem { get; set; }
     public Dictionary<string, ScoreCodeSummary> ScoreCodesUsed { get; set; }
 }
