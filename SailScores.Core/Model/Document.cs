2b863830a6f03e3aba93f02441ed4b98da1b2a81
James Fraser <jamie@widernets.com>
2025-05-26 13:23:42 -0500

    Regatta race connection failing sometimes
    This didn't seem to fix it. Still looking.

--- SailScores.Core/Model/Document.cs
+++ SailScores.Core/Model/Document.cs
@@ -1,29 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.IO;
 
-namespace SailScores.Core.Model
+namespace SailScores.Core.Model;
+
+public class Document
 {
-    public class Document
-    {
-        public Guid Id { get; set; }
-        public Guid ClubId { get; set; }
-        public Guid? RegattaId { get; set; }
+    public Guid Id { get; set; }
+    public Guid ClubId { get; set; }
+    public Guid? RegattaId { get; set; }
 
-        [StringLength(128)]
-        public String Name { get; set; }
+    [StringLength(128)]
+    [Required]
+    public String Name { get; set; }
 
-        [StringLength(128)]
-        public String ContentType { get; set; }
-        public Byte[] FileContents { get; set; }
+    [StringLength(128)]
+    public String ContentType { get; set; }
+    public Byte[] FileContents { get; set; }
 
-        public DateTime CreatedDate { get; set; }
-        public DateTime CreatedLocalDate { get; set; }
-        [StringLength(128)]
-        public String CreatedBy { get; set; }
-        public Guid? PreviousVersion { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime CreatedLocalDate { get; set; }
+    [StringLength(128)]
+    public String CreatedBy { get; set; }
+    public Guid? PreviousVersion { get; set; }
 
-    }
 }
