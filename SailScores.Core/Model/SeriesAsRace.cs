f55684f6f7d0fca062f60e0d0a36b2bc478fdb38
James Fraser <jamie@widernets.com>
2025-06-17 07:02:41 -0500

    WIP

---
+++ SailScores.Core/Model/SeriesAsRace.cs
@@ -0,0 +1,15 @@
+﻿using SailScores.Api.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SailScores.Core.Model
+{
+    public class SeriesAsRace : Race
+    {
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int TotalChildRaceCount { get; set; } = 0;
+
+    }
+}
