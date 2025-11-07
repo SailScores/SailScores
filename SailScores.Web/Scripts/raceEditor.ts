e7b99914d6e7d24e29beeb80beac0f1aab3854be
James Fraser <jamie@widernets.com>
2025-11-06 19:43:39 -0600

    Typescript fix

--- SailScores.Web/Scripts/raceEditor.ts
+++ SailScores.Web/Scripts/raceEditor.ts
@@ -240,7 +240,8 @@ export function confirmDelete() {
 
     var btn = <Node>event.target;
     var resultItem = $(btn).closest("li");
-    var compId = resultItem[0].dataset.competitorid;
+    var listElement = resultItem.get(0) as HTMLLIElement;
+    var compId = listElement.dataset.competitorid;
     var compName = resultItem.find(".competitor-name").text();
     if (!compName) {
         compName = resultItem.find(".sail-number").text();
