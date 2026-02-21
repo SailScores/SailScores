8a596c2e6b1e5fcc0a587958eccf84eb423651c5
James Fraser <jamie@widernets.com>
2026-02-21 06:29:49 -0600

    Restore fix for weather and files

--- SailScores.Database/SailscoresContext.cs
+++ SailScores.Database/SailscoresContext.cs
@@ -38,6 +38,8 @@ public class SailScoresContext : DbContext, ISailScoresContext
     public DbSet<File> Files { get; set; }
     public DbSet<Document> Documents { get; set; }
 
+    public DbSet<Weather> Weather { get; set; }
+
     public DbSet<HistoricalResults> HistoricalResults { get; set; }
     public DbSet<SeriesChartResults> SeriesChartResults { get; set; }
     public DbSet<SeriesForwarder> SeriesForwarders { get; set; }
